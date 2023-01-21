034b247905256e8484b2977dcdeab1b46f7e6554
MatKollar <matej.kollar3@gmail.com>
2023-01-21 13:11:05 +0100

    Added Binarization

--- frontend/src/components/PreprocessingCard/PreprocessingCard.js
+++ frontend/src/components/PreprocessingCard/PreprocessingCard.js
@@ -9,10 +9,23 @@ const PreprocessingCard = () => {
   const ocrCtx = useContext(OCRContext);
 
   const handleGrayScale = () => {
-    const mat = cv.imread("output");
-    cv.cvtColor(mat, mat, cv.COLOR_RGBA2GRAY, 0);
-    cv.imshow("output", mat);
-    mat.delete();
+    const src = cv.imread("output");
+    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+    cv.imshow("output", src);
+    src.delete();
+  };
+
+  const handleBinarization = () => {
+    console.log("asdasd");
+    let src = cv.imread("output");
+    let dst = new cv.Mat();
+    let thresholdValue = 0;
+    let maxValue = 255;
+    let thresholdType = cv.THRESH_OTSU;
+    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
+    cv.threshold(src, dst, thresholdValue, maxValue, thresholdType);
+    cv.imshow("output", dst);
+    src.delete();
   };
 
   return (
@@ -30,6 +43,15 @@ const PreprocessingCard = () => {
               sx={{ margin: "20px", px: "10%" }}
             >
               Grayscale
+            </Button>
+          </Grid>
+          <Grid item xs={3}>
+            <Button
+              variant="contained"
+              onClick={handleBinarization}
+              sx={{ margin: "20px", px: "10%" }}
+            >
+              Binarization
             </Button>
           </Grid>
         </Grid>
