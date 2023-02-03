eceb0160dc5d74cef4c46c8edc152a2ad2af938d
matej <xkollarm2@stuba.sk>
2023-02-03 17:54:14 +0100

    Show and hide text field with button

--- frontend/src/components/SummaryCard/SummaryCard.js
+++ frontend/src/components/SummaryCard/SummaryCard.js
@@ -1,24 +1,36 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import OCRContext from "../../context/ocr-context";
-import { TextField } from "@mui/material";
+import { TextField, Button } from "@mui/material";
 import { useStyles } from "./styles";
 
 const SummaryCard = () => {
   const classes = useStyles();
   const ocrCtx = useContext(OCRContext);
+  const [showText, setShowText] = useState(true);
 
   return (
     <>
       <div className={classes.rootContainer}>
-        <TextField
-          id="outlined-multiline-static"
-          className={classes.textField}
-          label="Text from OCR"
-          multiline
-          rows={20}
-          variant={"filled"}
-          defaultValue={ocrCtx.textResult}
-        />
+        {showText && (
+          <TextField
+            id="outlined-multiline-static"
+            className={classes.textField}
+            label="Text from OCR"
+            multiline
+            fullWidth
+            rows={20}
+            variant={"filled"}
+            defaultValue={ocrCtx.textResult}
+          />
+        )}
+
+        <Button
+          variant="contained"
+          onClick={() => setShowText(!showText)}
+          sx={{ margin: "5px", px: "10%" }}
+        >
+          {showText ? "HIDE TEXT" : "SHOW TEXT"}
+        </Button>
       </div>
     </>
   );
--- frontend/src/components/SummaryCard/styles.js
+++ frontend/src/components/SummaryCard/styles.js
@@ -2,12 +2,12 @@ import { makeStyles } from "@mui/styles";
 
 export const useStyles = makeStyles({
   rootContainer: {
+    width: '50%',
     margin: "0 auto",
     marginTop: 30,
   },
   textField: {
     backgroundColor: "white",
     borderRadius: 10,
-    width: "50%",
   },
 });
