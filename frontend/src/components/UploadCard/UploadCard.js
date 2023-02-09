9f08458705adac61e3d5be4eeb99e6a34d041b25
matej <xkollarm2@stuba.sk>
2023-02-09 21:43:26 +0100

    Convert PDF to image on upload

--- frontend/src/components/UploadCard/UploadCard.js
+++ frontend/src/components/UploadCard/UploadCard.js
@@ -8,16 +8,72 @@ const UploadCard = () => {
   const classes = useStyles();
   const ocrCtx = useContext(OCRContext);
 
-  const handleImageUpload = (event) => {
-    const img = new Image();
-    img.onload = () => {
-      const mat = cv.imread(img);
-      cv.imshow("output", mat);
-      mat.delete();
-    };
-    img.src = URL.createObjectURL(event.target.files[0]);
-    ocrCtx.setOriginalImage(event.target.files[0]);
-    ocrCtx.setActualImage(event.target.files[0]);
+  const PDFJS = require("pdfjs-dist/webpack");
+
+  const readFileData = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        resolve(e.target.result);
+      };
+      reader.onerror = (err) => {
+        reject(err);
+      };
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const convertPdfToImages = async (file) => {
+    let image;
+    const data = await readFileData(file);
+    const pdf = await PDFJS.getDocument(data).promise;
+    const canvas = document.createElement("canvas");
+    for (let i = 0; i < pdf.numPages; i++) {
+      const page = await pdf.getPage(i + 1);
+      const viewport = page.getViewport({ scale: 1 });
+      const context = canvas.getContext("2d");
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+      await page.render({ canvasContext: context, viewport: viewport }).promise;
+      image = canvas.toDataURL();
+    }
+    canvas.remove();
+    return image;
+  };
+
+  const handleImageUpload = async (event) => {
+    const file = event.target.files[0];
+    console.log(file.name);
+    if (file.type === "application/pdf") {
+      const image = convertPdfToImages(event.target.files[0]);
+      image.then(async (image) => {
+        const img = new Image();
+        img.onload = () => {
+          const mat = cv.imread(img);
+          cv.imshow("output", mat);
+          mat.delete();
+        };
+        img.src = image;
+        const base64Response = await fetch(`${image}`);
+        const blob = await base64Response.blob();
+        let fileImage = new File([blob], file.name, {
+          type: "image/jpeg",
+          lastModified: new Date().getTime(),
+        });
+        ocrCtx.setOriginalImage(fileImage);
+        ocrCtx.setActualImage(fileImage);
+      });
+    } else {
+      const img = new Image();
+      img.onload = () => {
+        const mat = cv.imread(img);
+        cv.imshow("output", mat);
+        mat.delete();
+      };
+      img.src = URL.createObjectURL(event.target.files[0]);
+      ocrCtx.setOriginalImage(event.target.files[0]);
+      ocrCtx.setActualImage(event.target.files[0]);
+    }
   };
 
   return (
