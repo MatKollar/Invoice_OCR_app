ad5c9aa91f136071b85803443c84bd485609b94e
MatKollar <matej.kollar3@gmail.com>
2024-05-26 13:55:19 +0200

    Add language select dropdown

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,7 +1,14 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useLocation } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
-import { IconButton, Typography } from "@mui/material";
+import {
+  IconButton,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  Tooltip,
+} from "@mui/material";
 import ButtonContained from "../StyledComponents/ButtonContained";
 import AuthContext from "../../context/auth-context";
 import { useStyles } from "./styles";
@@ -12,6 +19,7 @@ const Navbar = (props) => {
   const classes = useStyles();
   const location = useLocation();
   const authContext = useContext(AuthContext);
+  const [language, setLanguage] = useState("en");
 
   const getPageTitle = () => {
     switch (location.pathname) {
@@ -26,6 +34,10 @@ const Navbar = (props) => {
       default:
         return "";
     }
+  };
+
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value);
   };
 
   const logoutUser = async () => {
@@ -62,6 +74,32 @@ const Navbar = (props) => {
         </Typography>
       </div>
       <div className={classes.rightButtons}>
+        <FormControl variant="outlined" size="small" sx={{ mr: 2 }}>
+          <Tooltip title="Invoice language" placement="left">
+            <Select
+              value={language}
+              onChange={handleLanguageChange}
+              sx={{
+                color: "white",
+                "& .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "white",
+                },
+                "&:hover .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "#854de0",
+                },
+                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                  borderColor: "#854de0",
+                },
+                "& .MuiSvgIcon-root": {
+                  color: "white",
+                },
+              }}
+            >
+              <MenuItem value="en">English</MenuItem>
+              <MenuItem value="sk">Slovak</MenuItem>
+            </Select>
+          </Tooltip>
+        </FormControl>
         <ProfileIcon />
         <ButtonContained
           className={classes.logout}
